b9357aff4256fe1af3638a13f205efad30075b29
Manogna <11239a094@kanchiuniv.ac.in>
2025-10-30 16:07:21 +0530

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter operator (+, -, *, /, %): ");
+        char op = sc.next().charAt(0);
+
+        double result = 0;
+        switch (op) {
+            case '+': result = a + b; break;
+            case '-': result = a - b; break;
+            case '*': result = a * b; break;
+            case '/': result = (b != 0) ? a / b : Double.NaN; break;
+            case '%': result = a % b; break;
+            default: System.out.println("Invalid operator!");
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
